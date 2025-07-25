d444e8926c37eea05e3e600ef055dcb58ac4410f
Eduardo Flores <eduardo.flores.c@outlook.es>
2025-07-25 00:43:15 -0300

    Arreglado error en productos y auth

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import productsRouter from "./src/routes/products.router.js";
-import authRouter from "./src/routes/auth.router.js";
+import express from 'express';
+import cors from 'cors';
+import productsRouter from './src/routes/products.router.js';
+import authRouter from './src/routes/auth.router.js';
+
+
 
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -12,13 +15,14 @@ app.use(express.json());
 app.use(authRouter);
 app.use("/api", productsRouter);
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("<h1>Hola API Rest</h1>");
 });
 
-// Middleware 404
+// Middleware para manejar errores 404
 app.use((req, res, next) => {
-    res.status(404).json({ error: "Ruta no encontrada o recurso no encontrado" });
+    res.status(404).json({ error: 'Ruta no encontrada o recurso no encontrado' });
 });
 
-export default app;
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
