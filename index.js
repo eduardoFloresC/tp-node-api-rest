3f9667c0648ea416cdc6703b680cf0c1be15a1e6
Eduardo Flores <eduardo.flores.c@outlook.es>
2025-07-25 00:38:14 -0300

    se agrega vercell prueba2

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import "dotenv/config";
-import express from 'express';
-import cors from 'cors';
-import productsRouter from './src/routes/products.router.js';
-import authRouter from './src/routes/auth.router.js';
-
-
+import express from "express";
+import cors from "cors";
+import productsRouter from "./src/routes/products.router.js";
+import authRouter from "./src/routes/auth.router.js";
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
@@ -15,14 +12,13 @@ app.use(express.json());
 app.use(authRouter);
 app.use("/api", productsRouter);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("<h1>Hola API Rest</h1>");
 });
 
-// Middleware para manejar errores 404
+// Middleware 404
 app.use((req, res, next) => {
-    res.status(404).json({ error: 'Ruta no encontrada o recurso no encontrado' });
+    res.status(404).json({ error: "Ruta no encontrada o recurso no encontrado" });
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+export default app;
